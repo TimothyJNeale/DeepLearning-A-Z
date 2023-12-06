796ab69ce960d5f6d1ef015cb8478e1287b019d4
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-12-06 13:59:49 +0000

    Added data preprocessing

--- ANN/customer_churn_prediction.py
+++ ANN/customer_churn_prediction.py
@@ -38,10 +38,43 @@ logging.info(os.getcwd())
 logging.info('Main section entered')
 logging.info(tf.__version__)
 
-# import the dsts set
+# import the data set
 dataset = pd.read_csv(os.path.join(data_dir, DATA_FILE))
-X = dataset.iloc[0, 3:-1].values # ignore the first three columns
-y = dataset.iloc[0, -1].values
+X = dataset.iloc[:, 3:-1].values # ignore the first three columns, all rows
+y = dataset.iloc[:, -1].values # ignore all but the last column, all rows
+
+# Encode categorical data
+logging.info(type(X))
+logging.info(X[0:6, :])
+
+# Label encoding 'Gender' column
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+logging.info(type(X))
+logging.info(X[0:6, :])
+
+# One hot encoding 'Geography' column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+logging.info(type(X))
+logging.info(X[0:6, :])
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+# 80% of the data will be used for training, 20% for testing
+# random_state = 1 means the same random split will be used each time
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+# Scale the training set
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
 
 ######################################### FINISH ##############################################
 logging.info('End of program')
