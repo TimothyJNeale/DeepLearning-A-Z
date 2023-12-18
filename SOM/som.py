ed3c541c997efb128aa27e8008a7eb66a3bfa843
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-12-18 14:06:46 +0000

    Detect fraud

--- SOM/som.py
+++ SOM/som.py
@@ -13,7 +13,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+
 from minisom import MiniSom
+from pylab import bone, pcolor, colorbar, plot, show
 
 # load environment variables from .env file
 load_dotenv()
@@ -61,13 +63,31 @@ som.random_weights_init(X)
 som.train_random(data = X, num_iteration = 100)
 
 #################################### VISUALISING THE RESULTS ####################################
+logging.info('Visualising the results')
+bone()
+pcolor(som.distance_map().T) # distance_map() method returns all the mean interneuron distances in one 
+                             # matrix and we take the transpose of it
+colorbar()
+markers = ['o', 's'] # o - circle, s - square
+colors = ['r', 'g'] # r - red, g - green
+for i, x in enumerate(X):
+    w = som.winner(x) # returns the winning node for the customer x
+    plot(w[0] + 0.5, # x coordinate of the winning node
+         w[1] + 0.5, # y coordinate of the winning node
+         markers[y[i]], # marker type
+         markeredgecolor = colors[y[i]], # marker edge color
+         markerfacecolor = 'None', # marker face color
+         markersize = 10, # marker size
+         markeredgewidth = 2) # marker edge width
 
+show()
 
-   
+#################################### FINDING THE FRAUDS ##########################################
+logging.info('Finding the frauds')
 
-####################################### MAKING PREDICTIONS #######################################
-logging.info('Prediction section entered')
-
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(8,1)], mappings[(6,8)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
 
 
 ########################################### FINISH ###############################################
