1a158babda9b5cf9d2ae7a82f6a66c0e11e1d757
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2024-01-09 16:24:54 +0000

    Loaded data sets

--- BOL/boltzman_machine.py
+++ BOL/boltzman_machine.py
@@ -2,8 +2,10 @@
 
 WORKING_DIRECTORY = 'BOL'
 DATA_DIRECTORY ='data'
-TRAINING_DATA = 'Credit_Card_Applications.csv'
-MODEL_FILE = 'bolt.keras'  
+MOVIE_DATA = 'movies.dat'
+USER_DATA = 'users.dat'
+RATINGS_DATA = 'ratings.dat'
+MODEL_FILE = 'boltzman_model.pt'  
 
 # Import libraries
 import os
@@ -12,11 +14,12 @@ import logging
 
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler
-
-from minisom import MiniSom
-from pylab import bone, pcolor, colorbar, plot, show
+import torch    # Pytorch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
 
 # load environment variables from .env file
 load_dotenv()
@@ -31,13 +34,19 @@ logging.info('Start of program')
 
 # Get the current directory
 home = os.getcwd()
-# Gwt the data directory
+logging.info(f'Current directory: {home}')
+
+# Get the data directory
 data_dir = os.path.join(home, DATA_DIRECTORY)
 logging.info(f'Data directory: {data_dir}')
 
 # Get the training data
-training_data_file = os.path.join(data_dir, TRAINING_DATA)
-logging.info(f'Training data file: {training_data_file}')
+movie_data_file = os.path.join(data_dir, MOVIE_DATA)
+logging.info(f'Movie data file: {movie_data_file}')
+user_data_file = os.path.join(data_dir, USER_DATA)
+logging.info(f'User data file: {user_data_file}')
+ratings_data_file = os.path.join(data_dir, RATINGS_DATA)
+logging.info(f'User data file: {ratings_data_file}')
 
 # Get the working directory
 working_dir = os.path.join(home, WORKING_DIRECTORY)
@@ -48,7 +57,9 @@ logging.info(f'Current directory: {os.getcwd()}')
 if os.path.isfile(os.path.join(data_dir, MODEL_FILE)):
     logging.info(f'Model file {MODEL_FILE} exists')
     logging.info('Loading model')
-    bol = tf.keras.models.load_model(os.path.join(data_dir, MODEL_FILE))
+    # use PyTorch to load the model
+    bol = torch.load(os.path.join(data_dir, MODEL_FILE))
+
     logging.info('Model loaded')
 else:
     # Model file does not exist, build the model
@@ -57,6 +68,12 @@ else:
 
     ##################################### DATA PREPROCESSING ##########################################
     logging.info('Data preprocessing section entered')
+
+    # import the data sets
+    logging.info('Importing the data')
+    movies = pd.read_csv(movie_data_file, sep='::', header=None, engine='python', encoding='latin-1')
+    users = pd.read_csv(user_data_file, sep='::', header=None, engine='python', encoding='latin-1')
+    ratings = pd.read_csv(ratings_data_file, sep='::', header=None, engine='python', encoding='latin-1')
 
 
     ############################### TRAINING THE BOLTZMAN MACHINE ####################################
@@ -70,9 +87,10 @@ else:
 
 
 
-    # # Save the model
-    # bol.save(os.path.join(data_dir, MODEL_FILE))
-    # logging.info(f'Model saved as {MODEL_FILE}')
+    # # Save the modelvusing PyToroch
+    logging.info('Saving model')
+    torch.save(bol, os.path.join(data_dir, MODEL_FILE))
+
 
 #################################### MAKING RECOMENDATIONS #######################################
 
