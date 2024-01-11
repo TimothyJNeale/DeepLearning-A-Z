c95c0db1b13868095d039367345b0352cf01e91c
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2024-01-11 15:58:38 +0000

    Common data preprocessing completed

--- BOL/boltzman_machine.py
+++ BOL/boltzman_machine.py
@@ -6,6 +6,8 @@ MOVIE_DATA = 'movies.dat'
 USER_DATA = 'users.dat'
 RATINGS_DATA = 'ratings.dat'
 MODEL_FILE = 'boltzman_model.pt'  
+TRAINING_DATA = 'u1.base'
+TEST_DATA = 'u1.test'
 
 # Import libraries
 import os
@@ -48,10 +50,20 @@ logging.info(f'User data file: {user_data_file}')
 ratings_data_file = os.path.join(data_dir, RATINGS_DATA)
 logging.info(f'User data file: {ratings_data_file}')
 
+# Get the training data
+training_data_file = os.path.join(data_dir, TRAINING_DATA)
+logging.info(f'Training data file: {training_data_file}')
+
+# Get the test data
+test_data_file = os.path.join(data_dir, TEST_DATA)
+logging.info(f'Test data file: {test_data_file}')
+
 # Get the working directory
 working_dir = os.path.join(home, WORKING_DIRECTORY)
 os.chdir(working_dir)
 logging.info(f'Current directory: {os.getcwd()}')
+
+
 
 # Check if model file exists
 if os.path.isfile(os.path.join(data_dir, MODEL_FILE)):
@@ -64,6 +76,8 @@ if os.path.isfile(os.path.join(data_dir, MODEL_FILE)):
 else:
     # Model file does not exist, build the model
     logging.info(f'Model file {MODEL_FILE} does not exist')
+    bol= None
+
     logging.info('Building model')
 
     ##################################### DATA PREPROCESSING ##########################################
@@ -75,26 +89,59 @@ else:
     users = pd.read_csv(user_data_file, sep='::', header=None, engine='python', encoding='latin-1')
     ratings = pd.read_csv(ratings_data_file, sep='::', header=None, engine='python', encoding='latin-1')
 
+    # Import the training and test sets
+    logging.info('Importing the training and test sets')
+    training_set = pd.read_csv(training_data_file, delimiter='\t', header=None)
+    training_set = np.array(training_set, dtype='int')
+
+    test_set = pd.read_csv(test_data_file, delimiter='\t', header=None)
+    test_set = np.array(test_set, dtype='int')
+
+    # Get the number of users and movies
+    logging.info('Getting the number of users and movies')
+    num_users = int(max(max(training_set[:, 0]), max(test_set[:, 0])))
+    num_movies = int(max(max(training_set[:, 1]), max(test_set[:, 1])))
+    logging.info(f'Number of users: {num_users}')
+    logging.info(f'Number of movies: {num_movies}')
+
+    # Convert the data into an array with users in lines and movies in columns
+    logging.info('Converting the data into an array with users in lines and movies in columns')
+    def convert(data):
+        new_data = []
+        for id_users in range(1, num_users + 1):
+            id_movies = data[:, 1][data[:, 0] == id_users]
+            id_ratings = data[:, 2][data[:, 0] == id_users]
+            ratings = np.zeros(num_movies)
+            ratings[id_movies - 1] = id_ratings
+            new_data.append(list(ratings))
+        return new_data
+    
+    training_set = convert(training_set)
+    test_set = convert(test_set)
+
+    # Convert the data into Torch tensors
+    logging.info('Converting the data into Torch tensors')
+    training_set = torch.FloatTensor(training_set)
+    test_set = torch.FloatTensor(test_set)
+    
+    
 
     ############################### TRAINING THE BOLTZMAN MACHINE ####################################
     logging.info('Training the Boltzman Machine')
 
 
-    
 
     ################################ TRAINING THE BOLTZMAN MACHINE #######################################
     logging.info('Boltzman Machine training section entered')
 
-
-
-    # # Save the modelvusing PyToroch
-    logging.info('Saving model')
-    torch.save(bol, os.path.join(data_dir, MODEL_FILE))
+    # Save the model using PyTorch
+    # logging.info('Saving model')
+    # torch.save(bol, os.path.join(data_dir, MODEL_FILE))
 
 
 #################################### MAKING RECOMENDATIONS #######################################
 
-logging.info('MaKing Recomendations')
+logging.info('Making Recomendations')
 
 
 
